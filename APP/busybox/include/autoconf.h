6acd2b51fa1c1f23462537558fed97efcb37b8a8
sfstudio <sfstudio@mail.ru>
2009-03-28 20:19:33 +0600

    dropbear up to current version

--- APP/busybox/include/autoconf.h
+++ APP/busybox/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.13.3.-wive-ng.sf.net
  */
-#define AUTOCONF_TIMESTAMP "2009-03-27 18:42:15 OMST"
+#define AUTOCONF_TIMESTAMP "2009-03-28 19:26:09 OMST"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
