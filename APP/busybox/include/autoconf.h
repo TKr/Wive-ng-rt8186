62a2327e137f108cd4247ea405d1dbf8c4b7281d
sfstudio <sfstudio@mail.ru>
2009-05-14 00:53:34 +0700

    bas 4.0.17 compat

--- APP/busybox/include/autoconf.h
+++ APP/busybox/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.14.0.-wive-ng.sf.net
  */
-#define AUTOCONF_TIMESTAMP "2009-05-12 01:13:09 OMSST"
+#define AUTOCONF_TIMESTAMP "2009-05-14 00:39:24 OMSST"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
