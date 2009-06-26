f4159ce3b5acc7aaa0140446d592d97ed1b05cfa
sfstudio <sfstudio@mail.ru>
2009-06-26 16:54:11 +0700

    busybox up to 1.14.2

---
+++ APP/busybox/include/applet_tables.h
@@ -0,0 +1,300 @@
+/* This is a generated file, don't edit */
+
+#define NUM_APPLETS 96
+
+const char applet_names[] ALIGN1 = ""
+"[" "\0"
+"[[" "\0"
+"arp" "\0"
+"arping" "\0"
+"ash" "\0"
+"awk" "\0"
+"cat" "\0"
+"chmod" "\0"
+"clear" "\0"
+"cp" "\0"
+"crond" "\0"
+"crontab" "\0"
+"cut" "\0"
+"date" "\0"
+"dd" "\0"
+"df" "\0"
+"dhcprelay" "\0"
+"dmesg" "\0"
+"dumpleases" "\0"
+"echo" "\0"
+"egrep" "\0"
+"expr" "\0"
+"false" "\0"
+"fgrep" "\0"
+"find" "\0"
+"free" "\0"
+"ftpget" "\0"
+"ftpput" "\0"
+"grep" "\0"
+"gunzip" "\0"
+"gzip" "\0"
+"halt" "\0"
+"head" "\0"
+"hexdump" "\0"
+"hostname" "\0"
+"ifconfig" "\0"
+"inetd" "\0"
+"init" "\0"
+"insmod" "\0"
+"ip" "\0"
+"ipaddr" "\0"
+"iplink" "\0"
+"iproute" "\0"
+"iprule" "\0"
+"kill" "\0"
+"killall" "\0"
+"klogd" "\0"
+"linuxrc" "\0"
+"ln" "\0"
+"logger" "\0"
+"login" "\0"
+"logread" "\0"
+"ls" "\0"
+"lsmod" "\0"
+"microcom" "\0"
+"mkdir" "\0"
+"mknod" "\0"
+"modprobe" "\0"
+"mount" "\0"
+"mv" "\0"
+"netstat" "\0"
+"nice" "\0"
+"nslookup" "\0"
+"passwd" "\0"
+"ping" "\0"
+"poweroff" "\0"
+"ps" "\0"
+"reboot" "\0"
+"rm" "\0"
+"rmmod" "\0"
+"route" "\0"
+"run-parts" "\0"
+"sed" "\0"
+"seq" "\0"
+"sh" "\0"
+"sleep" "\0"
+"sort" "\0"
+"sysctl" "\0"
+"syslogd" "\0"
+"tail" "\0"
+"tar" "\0"
+"test" "\0"
+"top" "\0"
+"traceroute" "\0"
+"true" "\0"
+"udhcpc" "\0"
+"udhcpd" "\0"
+"umount" "\0"
+"uname" "\0"
+"uptime" "\0"
+"vconfig" "\0"
+"vi" "\0"
+"watch" "\0"
+"which" "\0"
+"yes" "\0"
+"zcat" "\0"
+;
+
+int (*const applet_main[])(int argc, char **argv) = {
+test_main,
+test_main,
+arp_main,
+arping_main,
+ash_main,
+awk_main,
+cat_main,
+chmod_main,
+clear_main,
+cp_main,
+crond_main,
+crontab_main,
+cut_main,
+date_main,
+dd_main,
+df_main,
+dhcprelay_main,
+dmesg_main,
+dumpleases_main,
+echo_main,
+grep_main,
+expr_main,
+false_main,
+grep_main,
+find_main,
+free_main,
+ftpgetput_main,
+ftpgetput_main,
+grep_main,
+gunzip_main,
+gzip_main,
+halt_main,
+head_main,
+hexdump_main,
+hostname_main,
+ifconfig_main,
+inetd_main,
+init_main,
+insmod_main,
+ip_main,
+ipaddr_main,
+iplink_main,
+iproute_main,
+iprule_main,
+kill_main,
+kill_main,
+klogd_main,
+init_main,
+ln_main,
+logger_main,
+login_main,
+logread_main,
+ls_main,
+lsmod_main,
+microcom_main,
+mkdir_main,
+mknod_main,
+modprobe_main,
+mount_main,
+mv_main,
+netstat_main,
+nice_main,
+nslookup_main,
+passwd_main,
+ping_main,
+halt_main,
+ps_main,
+halt_main,
+rm_main,
+rmmod_main,
+route_main,
+run_parts_main,
+sed_main,
+seq_main,
+ash_main,
+sleep_main,
+sort_main,
+sysctl_main,
+syslogd_main,
+tail_main,
+tar_main,
+test_main,
+top_main,
+traceroute_main,
+true_main,
+udhcpc_main,
+udhcpd_main,
+umount_main,
+uname_main,
+uptime_main,
+vconfig_main,
+vi_main,
+watch_main,
+which_main,
+yes_main,
+gunzip_main,
+};
+const uint16_t applet_nameofs[] ALIGN2 = {
+0x3000,
+0x3002,
+0x0005,
+0x0009,
+0x0010,
+0x2014,
+0x3018,
+0x201c,
+0x0022,
+0x2028,
+0x002b,
+0x8031,
+0x2039,
+0x003d,
+0x2042,
+0x0045,
+0x0048,
+0x0052,
+0x0058,
+0x3063,
+0x0068,
+0x006e,
+0x3073,
+0x0079,
+0x207f,
+0x0084,
+0x0089,
+0x0090,
+0x0097,
+0x009c,
+0x00a3,
+0x00a8,
+0x00ad,
+0x20b2,
+0x00ba,
+0x00c3,
+0x00cc,
+0x00d2,
+0x00d7,
+0x00de,
+0x00e1,
+0x00e8,
+0x00ef,
+0x00f7,
+0x00fe,
+0x0103,
+0x010b,
+0x0111,
+0x2119,
+0x011c,
+0x8123,
+0x0129,
+0x2131,
+0x0134,
+0x013a,
+0x3143,
+0x0149,
+0x014f,
+0x0158,
+0x015e,
+0x0161,
+0x0169,
+0x016e,
+0x8177,
+0x417e,
+0x0183,
+0x018c,
+0x018f,
+0x3196,
+0x0199,
+0x019f,
+0x01a5,
+0x01af,
+0x31b3,
+0x01b7,
+0x31ba,
+0x21c0,
+0x01c5,
+0x01cc,
+0x01d4,
+0x01d9,
+0x31dd,
+0x01e2,
+0x41e6,
+0x31f1,
+0x01f6,
+0x01fd,
+0x0204,
+0x020b,
+0x0211,
+0x0218,
+0x0220,
+0x0223,
+0x0229,
+0x322f,
+0x0233,
+};
+#define MAX_APPLET_NAME_LEN 10
