bbd84ee41c35d211de5935002d19123d38b42d94
sfstudio <sfstudio@mail.ru>
2009-11-12 02:18:47 +0600

    Add multicast probe tool mcprobe

--- APP/busybox/include/autoconf.h
+++ APP/busybox/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.15.2
  */
-#define AUTOCONF_TIMESTAMP "2009-11-10 01:04:58 OMST"
+#define AUTOCONF_TIMESTAMP "2009-11-10 03:49:09 OMST"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
