f95fa16b916f08a41908725b1a8c61d3d0d2ea66
sfstudio <sfstudio@mail.ru>
2009-11-20 17:20:29 +0600

    pppd up to 2.4.5 release

--- APP/busybox/include/autoconf.h
+++ APP/busybox/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.15.2
  */
-#define AUTOCONF_TIMESTAMP "2009-11-14 15:29:23 OMST"
+#define AUTOCONF_TIMESTAMP "2009-11-18 13:27:59 OMST"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
