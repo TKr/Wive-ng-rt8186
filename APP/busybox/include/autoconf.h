d3c228e35bfea5146005a9f598b7a8d0bcbc71ec
sfstudio <sfstudio@mail.ru>
2009-11-21 18:01:40 +0600

    Version up 0.3.11 (release for speedup version drivers and new ppp testing)

--- APP/busybox/include/autoconf.h
+++ APP/busybox/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.15.2
  */
-#define AUTOCONF_TIMESTAMP "2009-11-18 13:27:59 OMST"
+#define AUTOCONF_TIMESTAMP "2009-11-21 17:27:55 OMST"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
