d1f0003117de09083f18ab6ed9270331b7ad114c
sfstudio <sfstudio@mail.ru>
2009-11-23 02:57:50 +0600

    Add route manipulation support to xl2tpd

--- APP/busybox/include/autoconf.h
+++ APP/busybox/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.15.2
  */
-#define AUTOCONF_TIMESTAMP "2009-11-21 18:04:20 OMST"
+#define AUTOCONF_TIMESTAMP "2009-11-21 22:59:54 OMST"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
