ececc3fe50a2bc2f045370bb0d947f6a8935db29
sfstudio <sfstudio@mail.ru>
2009-11-24 20:47:10 +0600

    Replace binary iwcontrol, auth to source code version

--- APP/busybox/include/autoconf.h
+++ APP/busybox/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.15.2
  */
-#define AUTOCONF_TIMESTAMP "2009-11-24 02:26:08 OMST"
+#define AUTOCONF_TIMESTAMP "2009-11-24 20:09:28 OMST"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
